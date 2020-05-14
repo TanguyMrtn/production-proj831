eafd64f1fee16aa1b660ca25513cf856b1ea505f
TanguyMrtn <tanguy.mariton38@gmail.com>
2020-05-14 22:43:55 +0200

    first push

--- www/main-es2015.js
+++ www/main-es2015.js
@@ -1189,7 +1189,7 @@ __webpack_require__.r(__webpack_exports__);
 
 const environment = {
     production: false,
-    url: 'https://polytech-student-api.herokuapp.com/'
+    url: 'https://polytech-student-api.herokuapp.com'
 };
 /*
  * For easier debugging in development mode, you can import the following file
