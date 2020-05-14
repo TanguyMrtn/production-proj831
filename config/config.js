f709ee48159b2254a4f8709f59880a519cebda0e
TanguyMrtn <tanguy.mariton38@gmail.com>
2020-05-14 22:18:56 +0200

    first push

---
+++ config/config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    jwtSecret: 'un-password-tres-complique-meme-pour-KV',
+    // db: 'mongodb://localhost:27017/studentLifeD'
+    db:"mongodb+srv://user:polytech123456@cluster0-0a5mo.gcp.mongodb.net/test?retryWrites=true&w=majority"
+};
