27893d40f1bf65760c02114b39d0625a18143840
TanguyMrtn <tanguy.mariton38@gmail.com>
2020-05-15 14:17:00 +0200

    new deployement

--- app.js
+++ app.js
@@ -4,12 +4,9 @@
 
 var express     = require('express');
 var bodyParser  = require('body-parser');
-var passport	= require('passport');
-var mongoose    = require('mongoose');
-var config      = require('./config/config');
 var port        = process.env.PORT || 5000;
 var cors        = require('cors');
-var cron = require('node-cron');
+
 var app = express();
 const {spawn} = require('child_process');
 app.use(cors());
@@ -21,41 +18,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Use the passport package in our application
-app.use(passport.initialize());
-var passportMiddleware = require('./middleware/passport');
-passport.use(passportMiddleware);
-
-
-//Injection dataToBdd to the DB (for prod) ...
-// initData.injectBDD();
-
 
 // Demo Route (GET http://localhost:5000)
 app.get('/', function(req, res) {
   return res.send('Hello! The API is at http://localhost:' + port + '/api');
-});
-
-var routes = require('./api/routes');
-app.use('/api',routes);
-
-
-
-
-
-
-
-mongoose.connect(config.db, { useNewUrlParser: true , useCreateIndex: true,useUnifiedTopology: true,dbName:"studentLifeD"});
-const connection = mongoose.connection;
-
-
-
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully!');
-});
-
-connection.on('error', (err) => {
-  console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
-  process.exit();
 });
 
 // Start the server
--- config/config.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = {
-    jwtSecret: 'un-password-tres-complique-meme-pour-KV',
-    // db: 'mongodb://localhost:27017/studentLifeD'
-    db:"mongodb+srv://user:polytech123456@cluster0-0a5mo.gcp.mongodb.net/test?retryWrites=true&w=majority"
-};
